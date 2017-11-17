c93c12308711aba000f6142926e0266fce86642a
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 12:12:12 +0100

    feat(PallidaOrientationExam): add SearchResult ViewModel

---
+++ PallidaOrientationExam/PallidaOrientationExam/ViewModels/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿using PallidaOrientationExam.Models;
+using System.Collections.Generic;
+
+namespace PallidaOrientationExam.ViewModels
+{
+    public class SearchResult
+    {
+        public List<LicensePlate> LicensePlates { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
