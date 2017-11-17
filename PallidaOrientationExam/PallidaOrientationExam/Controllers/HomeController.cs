d3c1792f64db23d2c7cd865c0151621505fa52b1
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 09:39:18 +0100

    feat(PallidaOrientationExam): add basic library structure, add HttpGet endpoints
    Libraries:
    - Controllers
    - Views
    - Views\Home
    - Models
    - ViewModels
    - Entities
    - Repositories
    - Services
    - Collections
    
    Endpoints:
    - SearchResultsNormal
    - SearchResultsPolice
    - SearchResultsDiplomat

---
+++ PallidaOrientationExam/PallidaOrientationExam/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PallidaOrientationExam.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult SearchForm()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("search/{searchString}")]
+        public IActionResult SearchResultsNormal(string searchString)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("search/police/{searchString}")]
+        public IActionResult SearchResultsPolice(string searchString)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("search/diplomat/{searchString}")]
+        public IActionResult SearchResultsDiplomat(string searchString)
+        {
+            return View();
+        }
+    }
+}
