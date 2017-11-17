ebb4e30cfe97064d1bb2bdd427546d2b7f146a06
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 11:49:04 +0100

    feat(PallidaOrientationExam): add new search functions to PallidaOrientationExamRepository
    - SearchNormalCarsByPlate(string)
    - SearchResultsPolice()
    - SearchResultsDiplomat()
    - SearchResultsBrand(string)

--- PallidaOrientationExam/PallidaOrientationExam/Repositories/PallidaOrientationExamRepository.cs
+++ PallidaOrientationExam/PallidaOrientationExam/Repositories/PallidaOrientationExamRepository.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using PallidaOrientationExam.Entities;
+using PallidaOrientationExam.Models;
 
 namespace PallidaOrientationExam.Repositories
 {
@@ -10,7 +10,28 @@ namespace PallidaOrientationExam.Repositories
     {
         private PallidaOrientationExamContext pallidaOrientationExamContext;
 
-        public PallidaOrientationExamRepository(PallidaOrientationExamContext pallidaOrientationExamContext) 
+        public PallidaOrientationExamRepository(PallidaOrientationExamContext pallidaOrientationExamContext)
             => this.pallidaOrientationExamContext = pallidaOrientationExamContext;
+
+        public List<LicensePlate> SearchNormalCarsByPlate(string searchString)
+            => (from plate in pallidaOrientationExamContext.LicensePlates
+                where plate.Plate.Contains(searchString)
+                select plate).ToList();
+
+        public List<LicensePlate> SearchResultsPolice()
+            => (from plate in pallidaOrientationExamContext.LicensePlates
+                where plate.Plate.StartsWith("RB")
+                select plate).ToList();
+
+        public List<LicensePlate> SearchResultsDiplomat()
+            => (from plate in pallidaOrientationExamContext.LicensePlates
+                where plate.Plate.StartsWith("DT")
+                select plate).ToList();
+
+        public List<LicensePlate> SearchResultsBrand(string brand)
+            => (from plate in pallidaOrientationExamContext.LicensePlates
+                where plate.Brand.Equals(brand)
+                select plate).ToList();
+
     }
 }
