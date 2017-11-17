3169a5ef4bd3c0dd7dad770ca3b0bb6d94f8abfe
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 10:20:02 +0100

    feat(PallidaOrientationExam.Entities): add PallidaOrientationExamContext

---
+++ PallidaOrientationExam/PallidaOrientationExam/Entities/PallidaOrientationExamContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PallidaOrientationExam.Models;
+
+namespace PallidaOrientationExam.Entities
+{
+    public class PallidaOrientationExamContext :DbContext
+    {
+        public DbSet<LicensePlate> LicensePlates { get; private set; }
+
+        public PallidaOrientationExamContext(DbContextOptions<PallidaOrientationExamContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+    }
+}
