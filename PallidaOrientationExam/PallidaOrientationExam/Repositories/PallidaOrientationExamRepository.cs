a9da359bd5ff56be404097d358c06e900729b43d
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 10:30:12 +0100

    feat(PallidaOrientationExam): add PallidaOrientationExamRepository

---
+++ PallidaOrientationExam/PallidaOrientationExam/Repositories/PallidaOrientationExamRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PallidaOrientationExam.Entities;
+
+namespace PallidaOrientationExam.Repositories
+{
+    public class PallidaOrientationExamRepository
+    {
+        private PallidaOrientationExamContext pallidaOrientationExamContext;
+
+        public PallidaOrientationExamRepository(PallidaOrientationExamContext pallidaOrientationExamContext) 
+            => this.pallidaOrientationExamContext = pallidaOrientationExamContext;
+    }
+}
