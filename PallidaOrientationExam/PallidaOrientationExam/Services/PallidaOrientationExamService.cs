726d2c8cfb769e1f694932df6d297cc48813594c
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 10:30:32 +0100

    feat(PallidaOrientationExam): add PallidaOrientationExamService

---
+++ PallidaOrientationExam/PallidaOrientationExam/Services/PallidaOrientationExamService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PallidaOrientationExam.Repositories;
+
+namespace PallidaOrientationExam.Services
+{
+    public class PallidaOrientationExamService
+    {
+        private PallidaOrientationExamRepository pallidaOrientationExamRepository;
+
+        public PallidaOrientationExamService(PallidaOrientationExamRepository pallidaOrientationExamRepository) 
+            => this.pallidaOrientationExamRepository = pallidaOrientationExamRepository;
+    }
+}
