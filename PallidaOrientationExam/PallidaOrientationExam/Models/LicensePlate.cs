a663057d847b10836dc07db137c795cc9bfa260c
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 10:14:26 +0100

    feat(PallidaOrientationExam): add LicensePlates class to Models

---
+++ PallidaOrientationExam/PallidaOrientationExam/Models/LicensePlate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PallidaOrientationExam.Models
+{
+    public class LicensePlates
+    {
+        [Key]
+        [MaxLength(50)]
+        public string Plate { get; set; }
+        [MaxLength(50)]
+        public string Brand { get; set; }
+        [MaxLength(50)]
+        public string Model { get; set; }
+        [MaxLength(50)]
+        public string Color { get; set; }
+        public int Year { get; set; }
+    }
+}
