b6ec17d8b52eae17a0667c04d78b582d3340a36e
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 11:23:26 +0100

    feat(PallidaOrientationExam): add search to HomeController

--- PallidaOrientationExam/PallidaOrientationExam/Controllers/HomeController.cs
+++ PallidaOrientationExam/PallidaOrientationExam/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PallidaOrientationExam.Services;
+using PallidaOrientationExam.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PallidaOrientationExam.Controllers
 {
-    [Route("")]
+    [Route("/")]
     public class HomeController : Controller
     {
+        private PallidaOrientationExamService pallidaOrientationExamService;
+
+        public HomeController(PallidaOrientationExamService pallidaOrientationExamService) 
+            => this.pallidaOrientationExamService = pallidaOrientationExamService;
 
         [HttpGet]
         public IActionResult SearchForm(string searchString)
@@ -20,10 +26,17 @@ namespace PallidaOrientationExam.Controllers
         }
 
         [HttpGet]
-        [Route("search/{searchString?}")]
+        [Route("search/{searchString}")]
         public IActionResult SearchResultsNormal(string searchString)
         {
-            return View();
+            try
+            {
+                return View(pallidaOrientationExamService.SearchNormalCarsByPlate(searchString));
+            }
+            catch (Exception)
+            {
+                return View(new List<LicensePlate>());
+            }
         }
 
         [HttpGet]
