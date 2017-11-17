b6ed403c19e46fcc1a535457a8d69bd6616d8230
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 13:04:24 +0100

    feat(PallidaOrientationExam): add extra API endpoints (return JSONs)
    - police cars
    - diplomat cars

--- PallidaOrientationExam/PallidaOrientationExam/Controllers/APIController.cs
+++ PallidaOrientationExam/PallidaOrientationExam/Controllers/APIController.cs
@@ -21,5 +21,27 @@ namespace PallidaOrientationExam.Controllers
         [Route("search/{brand}")]
         public IActionResult SearchBrand(string brand) 
             => Json(pallidaOrientationExamService.SearchResultsBrand(brand));
+
+        [HttpGet]
+        [Route("search")]
+        public IActionResult Search()
+        {
+            // HMZ-140 or any part of the license plate (like MZ-1)
+            if (Request.Query.Count == 0)
+            {
+                return BadRequest("Nothing is searched for.");
+            }
+            else if (Request.Query.ContainsKey("police"))
+            {
+                return Json(pallidaOrientationExamService.SearchResultsPolice());
+            }
+            else if (Request.Query.ContainsKey("diplomat"))
+            {
+                return Json(pallidaOrientationExamService.SearchResultsDiplomat());
+            }
+
+            return NotFound();
+        }
+
     }
 }
