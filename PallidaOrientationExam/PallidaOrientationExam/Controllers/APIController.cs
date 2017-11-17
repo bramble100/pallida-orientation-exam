cd2663778851bda4f3d42799f18ffaf41d3263cd
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 12:06:05 +0100

    feat(PallidaOrientationExam): add APIController
    - add SearchBrand(string)

---
+++ PallidaOrientationExam/PallidaOrientationExam/Controllers/APIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PallidaOrientationExam.Services;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PallidaOrientationExam.Controllers
+{
+    [Route("api")]
+    public class APIController : Controller
+    {
+        private PallidaOrientationExamService pallidaOrientationExamService;
+
+        public APIController(PallidaOrientationExamService pallidaOrientationExamService)
+            => this.pallidaOrientationExamService = pallidaOrientationExamService;
+
+        [HttpGet]
+        [Route("search/{brand}")]
+        public IActionResult SearchBrand(string brand) 
+            => Json(pallidaOrientationExamService.SearchResultsBrand(brand));
+    }
+}
