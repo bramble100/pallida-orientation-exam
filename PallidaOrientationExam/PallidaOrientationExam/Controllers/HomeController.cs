7518268b523fd1dfea23e402d9c9b20a3a78f7d6
Arnold Barna <arnold.barna@outlook.com>
2017-11-17 13:13:31 +0100

    feat(PallidaOrientationExam): add feats to HomeController
    - add "police=1" search string
    - add "diplomat=1" search string
    - other minor changes

--- PallidaOrientationExam/PallidaOrientationExam/Controllers/HomeController.cs
+++ PallidaOrientationExam/PallidaOrientationExam/Controllers/HomeController.cs
@@ -5,52 +5,68 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PallidaOrientationExam.Services;
 using PallidaOrientationExam.Models;
+using PallidaOrientationExam.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PallidaOrientationExam.Controllers
 {
-    [Route("/")]
+    [Route("")]
     public class HomeController : Controller
     {
         private PallidaOrientationExamService pallidaOrientationExamService;
 
-        public HomeController(PallidaOrientationExamService pallidaOrientationExamService) 
+        public HomeController(PallidaOrientationExamService pallidaOrientationExamService)
             => this.pallidaOrientationExamService = pallidaOrientationExamService;
 
         [HttpGet]
-        public IActionResult SearchForm(string searchString)
+        public IActionResult Index(SearchResult searchResult)
         {
             // HMZ-140 or any part of the license plate (like MZ-1)
-            return View(searchString);
+            return View(searchResult ?? new SearchResult());
         }
 
         [HttpGet]
-        [Route("search/{searchString}")]
-        public IActionResult SearchResultsNormal(string searchString)
+        [Route("search")]
+        public IActionResult SearchResults()
         {
+            if (Request.Query.Count == 0)
+            {
+                return BadRequest("Nothing is searched for.");
+            }
+            else if (Request.Query.ContainsKey("police"))
+            {
+                return RedirectToAction("Index", pallidaOrientationExamService.SearchResultsPolice());
+            }
+            else if (Request.Query.ContainsKey("diplomat"))
+            {
+                return RedirectToAction("Index", pallidaOrientationExamService.SearchResultsDiplomat());
+            }
+
             try
             {
-                return View(pallidaOrientationExamService.SearchNormalCarsByPlate(searchString));
+                return View(pallidaOrientationExamService.SearchNormalCarsByPlate("2"));
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 return View(new List<LicensePlate>());
             }
         }
 
         [HttpGet]
-        [Route("/search/police")]
+        [Route("/police")]
         public IActionResult SearchResultsPolice()
-        {
-            return View();
-        }
+            => View(pallidaOrientationExamService.SearchResultsPolice());
 
         [HttpGet]
-        [Route("/search/diplomat")]
+        [Route("/diplomat")]
         public IActionResult SearchResultsDiplomat()
-        {
-            return View();
-        }
+            => View(pallidaOrientationExamService.SearchResultsDiplomat());
+
+        [HttpGet]
+        [Route("search/{brand}")]
+        public IActionResult SearchBrand(string brand)
+            => View(pallidaOrientationExamService.SearchResultsBrand(brand));
+
     }
 }
